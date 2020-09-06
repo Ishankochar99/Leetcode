4069abc41a90bf288e68e9a04ea8965b2ccbbfbb
Ishankochar99 <65892146+Ishankochar99@users.noreply.github.com>
2020-09-07 01:03:13 +0530

    Add files via upload
    commit

---
+++ Reverse words in a string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseWords(string &s) {
+        string result = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ' ') continue;
+            int pos = i;
+            while (i < s.length() && s[i] != ' ') i++;
+            if (result.length() > 0) result = ' ' + result;
+            result = s.substr(pos, i - pos) + result;
+            i--;
+        }
+        s = result;
+    }
+};
