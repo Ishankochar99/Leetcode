02cb3790b5be3c40313855cfa2b4535d0566f554
Ishankochar99 <65892146+Ishankochar99@users.noreply.github.com>
2021-03-10 23:12:30 +0530

    Add files via upload

---
+++ March Lettcode challenge/Week 1/Week 1/Average of Levels in Binary Tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double>res;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            long sum=0,count=0;
+            queue<TreeNode*>temp;
+            while(!q.empty()){
+                TreeNode* n=q.front();
+                q.pop();
+                sum+=n->val;
+                count++;
+                if(n->left!=NULL){
+                    temp.push(n->left);
+                }
+                if(n->right!=NULL){
+                    temp.push(n->right);
+                }
+            }
+            q=temp;
+            res.push_back((double)sum/ count);
+        }
+        return res;
+    }
+};
